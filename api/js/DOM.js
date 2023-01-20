e2b3d909b8b1645a1e8e4bedf21c85d254108f77
ashleylam30 <ashley.lam23@sitechhs.com>
2023-01-20 13:23:57 -0500

    dndfndfndfn

---
+++ api/js/DOM.js
@@ -0,0 +1,7 @@
+const DOM = {
+  btn: document.getElementById("btn"),
+  input: document.getElementById("input"),
+  page: document.getElementById("page"),
+};
+
+export { DOM };
--- api/js/main.js
+++ api/js/main.js
@@ -1,8 +1,22 @@
+import { DOM } from "../js/DOM.js";
+import "../styles/style.css";
+
 const link = `https://pokeapi.co/api/v2/pokemon/`;
 
 async function api(url) {
   const response = await fetch(url);
   const data = await response.json();
   console.log(data);
+  let input = DOM.input.value;
+  DOM.btn.addEventListener("click", function () {
+    DOM.page.insertAdjacentHTML(
+      "beforeend",
+      `<div>
+    <img src="${data.sprites.front_default}"><img>
+    </div>`
+    );
+  });
 }
 api(link);
+
+DOM.btn;
